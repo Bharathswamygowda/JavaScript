995cc84b5ce4c04b187731a0120853b4183d6a1b
Bharath <34337209+Bharathswamygowda@users.noreply.github.com>
2021-01-11 15:49:02 +0530

    Template Literals

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-const navSlide = () => {
-const mobileCTA = document.querySelector(".cta-menu");
-const nav = document.querySelector(".navigation");
+// Template Literals
+const name = "Bharath";
+const age = 28;
+const job = "Web Developer";
+const city = "Bangalore";
+let bioData;
 
-// Toggle Nav
-mobileCTA.addEventListener("click", () => {
-nav.classList.toggle("responsive");
-});
-};
+bioData = `
+<ul>
+<li>Name: ${name}</li>
+<li>Age: ${age}</li>
+<li>Job: ${job}</li>
+<li>City: ${city}</li>
+</ul>
+`;
 
-const app = () => {
-navSlide();
-};
-
-app();
-
-
-
-// carousel
-$('.testimonials-box-carasolue').owlCarousel({
-loop: false,
-dots: false,
-center: false,
-stagePadding: 10,
-nav: false,
-autoHeight: true,
-responsive: {
-0: {
-items: 1,
-margin: 20,
-
-},
-600: {
-items: 1,
-margin: 20,
-
-},
-1000: {
-items: 3,
-margin: 40
-}
-}
-})
+document.getElementById("biodata").innerHTML = bioData;
