56254e783e4c4bd8f9651b7b1c2721f18a707c7f
Bharath <34337209+Bharathswamygowda@users.noreply.github.com>
2021-01-11 16:14:35 +0530

    Object Literal

--- index.js
+++ index.js
@@ -14,4 +14,38 @@ bioData = `
 </ul>
 `;
 
-document.getElementById("biodata").innerHTML = bioData;
+document.getElementById("Template_literal").innerHTML = bioData;
+
+// Object literals
+const person = {
+  firstName: "Bharat",
+  age: 28,
+  email: "bharathsgowda111@gmail.com",
+  hobbies: ["eat", "BikeRiding", "Cricket"],
+  address: {
+    city: "Bangalore",
+    state: "karnataka"
+  },
+  getBirthYear: function () {
+    return 2020 - this.age;
+  }
+};
+
+let val;
+val = person;
+
+val = `
+<ul>
+<li>Frist Name: ${val.firstName}</li>
+<li>Age: ${val.age}</li>
+<li>Email: ${val.email}</li>
+<li>Hobbies: ${val.hobbies}</li>
+<li>Hobbies: ${val.hobbies[0]}</li>
+<li>Address: ${val.address.city}</li>
+<li>Address: ${val.address.state}</li>
+<li>Age: ${val.getBirthYear()}</li>
+</ul>
+
+`;
+
+document.getElementById("object_literal").innerHTML = val;
