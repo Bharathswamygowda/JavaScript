48509ce8b090f0ad11b99edb7d9f9c81ece1058e
Bharath <34337209+Bharathswamygowda@users.noreply.github.com>
2021-01-12 15:07:39 +0530

    conditional Operator

---
+++ Conitional_comparision.js
@@ -0,0 +1,30 @@
+// if(something){
+//   do something
+// }
+
+// else{
+//   do some thing else
+// }
+
+// = assigning the values
+// == check the values
+// === check the values and data types
+// && to check both the conditons
+// || to check any one conditions
+
+// ternary operator
+// id===100 ? "yes correct" : "no its not 100"
+
+const numb = 101;
+if (numb === 100) {
+  console.log("yes the number is 100");
+} else {
+  console.log("No the number is not 100");
+}
+
+const age1 = 25;
+if (age1 > 0 && age1 < 18) {
+  console.log("he is a child");
+} else {
+  console.log("he is a men");
+}
